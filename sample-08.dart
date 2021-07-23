3e8c8bab5417503bc05a44377f4d4dcb230409d4
kashiwara <tanakatarou0205@gmail.com>
2021-07-23 18:30:40 +0900

    Add sample-08

--- sample-08.dart
+++ sample-08.dart
@@ -1,18 +1,19 @@
-abstract class Event{
-  // constructorを持てない
-  
+class Event{
   String getName() => "name";
   String getTest() => "test";
 }
 
-// astract classによるDI
+// 親クラスを型にしても実行化
 void echoEventName(Event event){
   print( event.getName() );
 }
 
-void echoEventTest(Event event){
-  print( event.getTest() );
+// 親に実装されていないメソッドを叩くことは不可能
+/*
+void echoEventName(Event event){
+  print( event.getFuga() );
 }
+*/
 
 class AEvent extends Event {
   final String name;
@@ -20,11 +21,11 @@ class AEvent extends Event {
   AEvent(this.name);
 
   String getName() => this.name;
+  String getFuga() => this.name;
 }
 
 void main(){
   final aEvent = new AEvent("xxx");
   print(aEvent.getName());
   echoEventName(aEvent);
-  echoEventTest(aEvent);
 }
